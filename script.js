e6faa36eb8adad36e997ab6d9543c8565a6feec4
dobrqka <dobrin.andreev@gmail.com>
2023-10-06 12:46:53 +0300

    Set up fetch general info of current weather

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-const API_KEY = "fcfc5bf03eb543b5be0185337231909";
+// returns an object containing all the weather info
 
 const getData = async (location) => {
+  // parameters needed to create the URL for the API call
   const mainUrl = "http://api.weatherapi.com/v1/forecast.json";
-  const apiKey = "?key=" + API_KEY;
+  const API_KEY = "fcfc5bf03eb543b5be0185337231909";
+  const apiPart = "?key=" + API_KEY;
   const place = "&q=" + location;
   const numberOfDays = "&days=3";
-
-  const response = await fetch(`${mainUrl}${apiKey}${place}${numberOfDays}`, {
+  // API call to fetch json with weather data
+  const response = await fetch(`${mainUrl}${apiPart}${place}${numberOfDays}`, {
     mode: "cors",
   });
   const weatherData = await response.json();
-  console.log(weatherData);
+  return weatherData;
 };
 
-getData("Shumen");
+// uses the getData function to access its "current" property to
+// only fetch the current weather
+
+const getCurrent = async (location) => {
+  const mainWeatherObj = await getData(location);
+  const currentWeather = mainWeatherObj.current;
+
+  // object containining only summarized current weather info
+  const currentGeneral = {
+    temp: currentWeather.temp_c + "°C",
+    feels: currentWeather.feelslike_c + "°C",
+    wind: Math.round(currentWeather.wind_kph * 0.28) + "m/s", // converts km/h to m/s
+    rain: currentWeather.precip_mm + "mm",
+    general: currentWeather.condition.text,
+    icon: currentWeather.condition.icon,
+  };
+  console.log(currentGeneral);
+};
+
+getCurrent("Shumen");
